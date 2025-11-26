b10e74e95a52bd6e219fa241e5b2ce0bcb596a65
Arunkumar <arunkumarnura18@gmail.com>
2025-11-26 19:05:07 +0530

    Added a new doc

---
+++ variable and values/variable.js
@@ -0,0 +1,70 @@
+// ------------------------------ variable var ----------------------------------
+var user = "sarah";
+console.log(globalThis.hasOwnProperty("user"));
+
+console.log(globalThis);
+
+// Function
+
+// 🔹 Global Scope
+
+// A variable is in the global scope if it is declared outside any function or block.
+
+// 🔹 Function Scope
+
+// A variable is in function scope if it is declared inside a function using var, let, or const.
+
+function addNumber(a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+console.log(addNumber(5, 6));
+
+function sayHi() {
+  console.log(`${user} say hi!`);
+}
+sayHi();
+
+// ------------------------------- variable Let ----------------------------------
+
+// ✔️ Are block-scoped
+
+// This means the variable exists only inside the block { } where it was declared.
+
+// if statement
+
+var cost = 1000;
+
+if (cost >= 1000) {
+  // var cost = 950; --- override
+  let cost = 950;
+  console.log(`special clearence cost: ${cost}`);
+}
+
+console.log(`original cost: ${cost}`);
+
+// for loop
+
+var x = 200;
+
+// for (var x = 0; x < 10; x = x + 1) {.     var override
+for (let x = 0; x < 10; x = x + 1) {
+  console.log(`The number is ${x}`);
+}
+
+console.log(`Orginal Number is ${x}`);
+
+//
+
+// function dosomethimg() {
+//   console.log(uur);
+//   var urr = "arun";
+// }
+// dosomethimg();
+
+let a = 12;
+if (a > 10) {
+  console.log(nod);
+  let nod = "john";
+}

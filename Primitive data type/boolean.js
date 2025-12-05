5be4c06052fd9cf6cb12ade39fa8c8fed7591d1b
Arunkumar <arunkumarnura18@gmail.com>
2025-12-05 22:06:55 +0530

    updated

--- Primitive data type/boolean.js
+++ Primitive data type/boolean.js
@@ -11,27 +11,23 @@ var forceDelet = false;
 
 // double NOT operator;
 
-var message = "";
-
 var message = "hi";
-
-// -----------
 var showPostButton = false;
-// -----------
 var showPostButton = !!message;
+
+// ✅ What does !! mean in JavaScript?
+// The double exclamation mark (!!) is used to convert any value into a boolean (true or false).
 
 console.log(`show post button: ${showPostButton}`);
 
 var numOfCharacter = 0;
-
-var numOfCharacter = message.length;
+var numOfCharacter = numOfCharacter.length;
 var showEditButton = !!numOfCharacter;
 console.log(`Show Edit Button: ${showEditButton}`);
 
 // greater than or less than operator True or False
 
 var ageOfJoe = 19;
-
 var eligibleForVoting = ageOfJoe > 18;
 var isJoeASeniorCitizen = ageOfJoe > 60;
 
